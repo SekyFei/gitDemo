f85e46187b56da689f5153788632dfb57a9058f3
3315473229@qq.com <3315473229@qq.com>
2018-06-09 16:52:44 +0800

    新增方法

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -3,7 +3,11 @@ package com.tujia;
 public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111111111---dev1");
-		System.out.println("新建dev01分支...........................");
+		System.out.println("锟铰斤拷dev01锟斤拷支...........................");
 		System.out.println("1111111111111111111111111111---dev1");
+	}
+	
+	public void testMeth(){
+		System.out.println("鏂板鏂规硶................................");
 	}
 }
