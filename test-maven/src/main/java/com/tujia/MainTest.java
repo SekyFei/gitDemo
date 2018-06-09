fc5f1e108b1cc169af0e6e718f960c970de42d0e
3315473229@qq.com <3315473229@qq.com>
2018-06-09 17:03:20 +0800

    111111

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -4,12 +4,11 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111111111---dev1");
 		System.out.println("修改编码格式...........................");
-		System.out.println("修改编码格式...........................");
 		System.out.println("1111111111111111111111111111---dev1");
 	}
 	
 	public void testMeth(){
-		System.out.println("新增方法................................");
+		System.out.println("新增方法222222222222222222222222222222222");
 		System.out.println("新增方法222222222222222222222222222222222");
 	}
 }
