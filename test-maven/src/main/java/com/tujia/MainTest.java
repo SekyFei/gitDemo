38704db23899a820e332b0b416ef7a1486248124
3315473229@qq.com <3315473229@qq.com>
2018-06-09 16:46:33 +0800

    新建分支dev01

---
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -0,0 +1,9 @@
+package com.tujia;
+
+public class MainTest {
+	public static void main(String[] args) {
+		System.out.println("1111111111111111111111111111---dev1");
+		System.out.println("新建dev01分支...........................");
+		System.out.println("1111111111111111111111111111---dev1");
+	}
+}
