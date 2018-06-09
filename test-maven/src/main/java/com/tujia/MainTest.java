5873b0ad98505de13420eeed66453d0c48c13325
3315473229@qq.com <3315473229@qq.com>
2018-06-09 17:09:31 +0800

    修改main方法

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -2,7 +2,7 @@ package com.tujia;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("1111111111111111111111111111---dev1");
+		System.out.println("1111111111111111111111111111---dev22222222	");
 	}
 	
 	public void testMeth(){
