f70191bab6abe6d55441500a33fc7593bed577dc
3315473229@qq.com <3315473229@qq.com>
2018-06-09 16:57:54 +0800

    修改编码格式

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -4,6 +4,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111111111---dev1");
 		System.out.println("修改编码格式...........................");
+		System.out.println("修改编码格式...........................");
 		System.out.println("1111111111111111111111111111---dev1");
 	}
 	
