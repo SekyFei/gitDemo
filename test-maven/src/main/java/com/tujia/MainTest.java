76fe35801ad0812c0870cc7188c87f6cd0f9fdb2
3315473229@qq.com <3315473229@qq.com>
2018-06-09 18:09:29 +0800

    IDEA中修改代码

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -1,15 +1,20 @@
 package com.tujia;
 
 public class MainTest {
-	public static void main(String[] args) {
-		System.out.println("1111111111111111111111111111---dev22222222	");
-		System.out.println("1111111111111111111111111111---dev11111111");
-		System.out.println("111111111111111111111111111111111111111111");
-		System.out.println("111111111111111111111111111111111111111111");
-	}
-	
-	public void testMeth(){
-		System.out.println("新增方法222222222222222222222222222222222");
-		System.out.println("新增方法222222222222222222222222222222222");
-	}
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111111111111---dev22222222	");
+        System.out.println("1111111111111111111111111111---dev11111111");
+        System.out.println("111111111111111111111111111111111111111111");
+        System.out.println("111111111111111111111111111111111111111111");
+    }
+
+    public void testMeth(){
+        System.out.println("新增方法222222222222222222222222222222222");
+        System.out.println("新增方法222222222222222222222222222222222");
+    }
+    public void IDEAMeth(){
+        System.out.println("IDEAMethIDEAMethIDEAMeth------------");
+        System.out.println("IDEAMethIDEAMethIDEAMeth------------");
+        System.out.println("IDEAMethIDEAMethIDEAMeth------------");
+    }
 }
