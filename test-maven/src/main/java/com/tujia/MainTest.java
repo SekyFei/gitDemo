22b5298eacd4c91265068c7b424ad6802d2d2be5
3315473229@qq.com <3315473229@qq.com>
2018-06-09 16:50:25 +0800

    修改编码格式

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -3,7 +3,7 @@ package com.tujia;
 public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111111111111111---dev1");
-		System.out.println("新建dev01分支...........................");
+		System.out.println("淇敼缂栫爜鏍煎紡...........................");
 		System.out.println("1111111111111111111111111111---dev1");
 	}
 }
