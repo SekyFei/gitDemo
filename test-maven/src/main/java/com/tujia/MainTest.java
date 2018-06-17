0e73309da4b39e077d31106b1e6b337c72117cc0
3315473229@qq.com <3315473229@qq.com>
2018-06-17 12:31:16 +0800

    请求

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -3,5 +3,6 @@ package com.tujia;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("eclipse.....................");
+        System.out.println("111111111111111111111111111111");
     }
 }
