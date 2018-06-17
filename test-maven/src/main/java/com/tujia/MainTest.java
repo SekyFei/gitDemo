c48ad19ca80ffd513a3c59931f349ed494b31341
3315473229@qq.com <3315473229@qq.com>
2018-06-17 12:45:34 +0800

    11111

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -3,5 +3,8 @@ package com.tujia;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("eclipse.....................");
+        System.out.println("2222222222222222222222222222222");
+        System.out.println("2222222222222222222222222222222");
+        System.out.println("2222222222222222222222222222222");
     }
 }
