5a7092d3261771e1693b18d9eaa33b06daa5cff1
3315473229@qq.com <3315473229@qq.com>
2018-06-17 11:54:45 +0800

    idea里修改方法

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -1,4 +1,6 @@
-package com.tujia;
+package main.java.com.tujia;
+
+import java.io.File;
 
 public class MainTest {
     public static void main(String[] args) {
@@ -6,9 +8,13 @@ public class MainTest {
         System.out.println("1111111111111111111111111111---dev11111111");
         System.out.println("111111111111111111111111111111111111111111");
         System.out.println("111111111111111111111111111111111111111111");
+        System.out.println(1/0);
+        System.out.println("111111111111111111111111111111111111111111");
     }
 
     public void testMeth(){
+        System.out.println("新增方法222222222222222222222222222222222");
+        File file = new File("/");
         System.out.println("新增方法222222222222222222222222222222222");
         System.out.println("新增方法222222222222222222222222222222222");
     }
--- test-maven/src/test/java/com/tujia/Main.java
+++ test-maven/src/test/java/com/tujia/Main.java
@@ -3,5 +3,6 @@ package com.tujia;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1111111111111111");
+		File file = new File();
 	}
 }
