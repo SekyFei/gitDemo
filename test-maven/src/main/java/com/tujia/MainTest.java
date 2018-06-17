8c847cfc26db9ada8e906fead5798e93b2017f66
3315473229@qq.com <3315473229@qq.com>
2018-06-17 11:52:21 +0800

    删除方法

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -2,20 +2,6 @@ package com.tujia;
 
 public class MainTest {
 	public static void main(String[] args) {
-		System.out.println("1111111111111111111111111111---dev22222222	");
-		System.out.println("1111111111111111111111111111---dev11111111");
-		System.out.println("111111111111111111111111111111111111111111");
-		System.out.println("111111111111111111111111111111111111111111");
-	}
-
-	public void testMeth() {
-		System.out.println("新增方法222222222222222222222222222222222");
-		System.out.println("新增方法222222222222222222222222222222222");
-	}
-
-	public void eclipseTest() {
-		System.out.println("eclipseTest---------------------");
-		System.out.println("eclipseTest---------------------");
-		System.out.println("eclipseTest---------------------");
+		
 	}
 }
