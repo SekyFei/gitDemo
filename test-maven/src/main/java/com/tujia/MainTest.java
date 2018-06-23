6a767e081d2be886eb8043f4a06991c51faee012
3315473229@qq.com <3315473229@qq.com>
2018-06-23 17:19:21 +0800

    新建dev1开发分支:获取用户名

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -2,6 +2,6 @@ package com.tujia;
 
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("111111111111111111111");
+        System.out.println("1111111111111111111111");
     }
 }
