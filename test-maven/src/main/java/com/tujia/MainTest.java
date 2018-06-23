d65f0c41dd5b48f6bb797bf750266ba873232fc0
3315473229@qq.com <3315473229@qq.com>
2018-06-23 17:22:50 +0800

    新建dev1开发分支:代码2

--- test-maven/src/main/java/com/tujia/MainTest.java
+++ test-maven/src/main/java/com/tujia/MainTest.java
@@ -3,5 +3,6 @@ package com.tujia;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("1111111111111111111111");
+        System.out.println("222222222222222222222222");
     }
 }
